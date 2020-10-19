d3d84609aa7dafb86f9f0f27dfa96e50cf16f5f6
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-10-19 09:14:18 +0100

    Rearranging the sim panel

--- src/App/Simulator.tsx
+++ src/App/Simulator.tsx
@@ -29,6 +29,7 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "auto",
     flexDirection: "column",
     alignItems: "center",
+    position: "relative",
 
     "& > *": {
       marginBottom: "25px",
@@ -47,6 +48,11 @@ export default (props: SimulatorProps) => {
     codes: {},
     error: false,
     reset: false,
+    comparrison: {
+      valOne: "",
+      valTwo: "",
+      result: "",
+    }
   });
   console.log(programState);
   useMemo(() => {
@@ -59,8 +65,13 @@ export default (props: SimulatorProps) => {
       blockKey: "main",
       error: false,
       reset: false,
+      comparrison: {
+        valOne: "null",
+        valTwo: "null",
+        result: "null",
+      },
     });
-  }, [props.code, programState.reset]);
+  }, [props.code, programState.reset]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useMemo(() => {
     const element = document.querySelector<HTMLElement>(`.tln-wrapper`)!;
@@ -89,7 +100,11 @@ export default (props: SimulatorProps) => {
     return (
       <div className={classes.root}>
         {programState.error ? (
-          <Alert severity="error">I fucked up</Alert>
+          <Alert severity="error">
+            The current operation has not been implemented yet, please raise an
+            issue on GitHub or email me at mike@bruty.net and I'll get on it
+            asap
+          </Alert>
         ) : null}
         <Buttons
           setProgramState={setProgramState}
@@ -99,6 +114,78 @@ export default (props: SimulatorProps) => {
           setStarted={setStarted}
           started={started}
         />
+        <Paper
+          style={{
+            padding: "5px",
+            paddingLeft: "15px",
+            paddingRight: "15px",
+            width: "150%",
+          }}
+        >
+          <p>EIP pointing to line: {programState.currentLine}</p>
+        </Paper>
+        <Paper style={{ width: "200%" }}>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Variable Name</TableCell>
+                  <TableCell>Value(s)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {programState.registers
+                  .filter((x) => !x.register)
+                  .map((v) => (
+                    <TableRow>
+                      <TableCell>{v.name}</TableCell>
+                      <TableCell>
+                        {v.value.toString().includes(",")
+                          ? "[ " + v.value + " ]"
+                          : v.value}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+        <Paper style={{ width: "200%" }}>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Register Name</TableCell>
+                  <TableCell>Value</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {programState.registers
+                  .filter((x) => x.register)
+                  .map((register: IRegister) => (
+                    <TableRow>
+                      <TableCell>{register.name}</TableCell>
+                      <TableCell>{register.value}</TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+        <Paper
+          style={{
+            padding: "5px",
+            paddingLeft: "15px",
+            paddingRight: "15px",
+            width: "150%",
+          }}
+        >
+          <h4>Comparison:</h4>
+          <p>
+            {programState.comparrison.valOne}, {programState.comparrison.valTwo}
+          </p>
+          <p>Result: {programState.comparrison.result}</p>
+        </Paper>
         <p>Show numbers in hex?</p>
         <div style={{ display: "flex", justifyContent: "center" }}>
           <Grid component="label" container alignItems="center" spacing={1}>
@@ -113,78 +200,31 @@ export default (props: SimulatorProps) => {
             <Grid item>Yes</Grid>
           </Grid>
         </div>
-        <Paper
-          style={{ padding: "5px", paddingLeft: "15px", paddingRight: "15px" }}
-        >
-          <p>EIP pointing to line: {programState.currentLine}</p>
-        </Paper>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Variable Name</TableCell>
-                <TableCell>Value(s)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {programState.registers
-                .filter((x) => !x.register)
-                .map((v) => (
-                  <TableRow>
-                    <TableCell>{v.name}</TableCell>
-                    <TableCell>
-                      {v.value.toString().includes(",")
-                        ? "[ " + v.value + " ]"
-                        : v.value}
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Register Name</TableCell>
-                <TableCell>Value</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {programState.registers
-                .filter((x) => x.register)
-                .map((register: IRegister) => (
-                  <TableRow>
-                    <TableCell>{register.name}</TableCell>
-                    <TableCell>{register.value}</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Paper
-          style={{
-            padding: "5px",
-            paddingLeft: "15px",
-            paddingRight: "15px",
-          }}
-        >
-          <h4>Comparison:</h4>
-          <p>"r" === "r"</p>
-          <p>Result: true</p>
-        </Paper>
+        <p style={{ position: "absolute", bottom: 0, width: "200%" }}>
+          Note: This doesn't have any error checking built in, so please run it
+          through an actual compiler and make sure it runs to the end before
+          using this app or else it might break if you code doesn't work
+          properly
+        </p>
       </div>
     );
   }
 
   return (
-    <Buttons
-      setProgramState={setProgramState}
-      programState={programState}
-      setLocked={props.setLocked}
-      nextClick={nextClick}
-      started={started}
-      setStarted={setStarted}
-    />
+    <div className={classes.root}>
+      <Buttons
+        setProgramState={setProgramState}
+        programState={programState}
+        setLocked={props.setLocked}
+        nextClick={nextClick}
+        started={started}
+        setStarted={setStarted}
+      />
+      <p style={{ position: "absolute", bottom: 0, width: "200%" }}>
+        Note: This doesn't have any error checking built in, so please run it
+        through an actual compiler and make sure it runs to the end before using
+        this app or else it might break if you code doesn't work properly
+      </p>
+    </div>
   );
 };
